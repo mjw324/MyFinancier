3af7954e85682db0c27998add8e9ee08b901eff4
Micah Worth <micah.worth2@gmail.com>
2026-05-02 23:05:10 -0400

    removed example from sign up text

--- src/app/(routes)/(auth)/signup/page.tsx
+++ src/app/(routes)/(auth)/signup/page.tsx
@@ -10,8 +10,7 @@ export default function SignUpPage() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center p-10">
       <div className="flex w-full flex-col rounded-2xl border border-foreground/10 px-8 py-5 md:w-96">
-        <h1>Sign Up</h1>
-        <p>Example sign up page using Better Auth</p>
+        <h1>Sign Up - MyFinancier</h1>
         <SignUpForm />
         <div className="flex items-center justify-center gap-2">
           <small>Already have account?</small>
